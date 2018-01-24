7b61ba4ce6d4bbe85a7f9a7cf70853f59eda15eb
hathaitornr <hathaitornr@gmail.com>
2018-01-24 01:34:21 -0500

    Addressed code feedback ver2

--- app/src/main/java/com/example/android/foodquiz/ResultActivity.java
+++ app/src/main/java/com/example/android/foodquiz/ResultActivity.java
@@ -9,32 +9,25 @@ import com.example.android.foodquiz.result.QuizResults;
 
 public class ResultActivity extends AppCompatActivity {
 
+    private TextView scoreDisplay;
+
+    private TextView[] resultDisplays;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_activity);
 
+        findViews();
+
         QuizResults quizResults = getQuizResultsFromPreviousActivity();
         populateQuizResults(quizResults);
     }
 
-    private QuizResults getQuizResultsFromPreviousActivity() {
-        int score = getIntent().getIntExtra("score", 0);
-        boolean[] results = getIntent().getBooleanArrayExtra("results");
-        return new QuizResults(score, results);
-    }
+    private void findViews() {
+        scoreDisplay = (TextView) findViewById(R.id.score);
 
-    private boolean[] getResultsFromPreviousActivity() {
-        return getIntent().getBooleanArrayExtra("results");
-    }
-
-    private void populateQuizResults(QuizResults quizResults) {
-        TextView scoreDisplay = (TextView) findViewById(R.id.score);
-        scoreDisplay.setText(String.valueOf(quizResults.getScore()));
-
-        boolean[] results = quizResults.getResults();
-
-        TextView[] resultDisplays = new TextView[] {
+        resultDisplays = new TextView[] {
                 (TextView) findViewById(R.id.q1_ans),
                 (TextView) findViewById(R.id.q2_ans),
                 (TextView) findViewById(R.id.q3_ans),
@@ -46,6 +39,18 @@ public class ResultActivity extends AppCompatActivity {
                 (TextView) findViewById(R.id.q9_ans),
                 (TextView) findViewById(R.id.q10_ans)
         };
+    }
+
+    private QuizResults getQuizResultsFromPreviousActivity() {
+        int score = getIntent().getIntExtra("score", 0);
+        boolean[] results = getIntent().getBooleanArrayExtra("results");
+        return new QuizResults(score, results);
+    }
+
+    private void populateQuizResults(QuizResults quizResults) {
+        scoreDisplay.setText(String.valueOf(quizResults.getScore()));
+
+        boolean[] results = quizResults.getResults();
 
         for (int i = 0; i < resultDisplays.length; i++) {
             int resultResId = (results[i]) ? R.string.correct : R.string.incorrect;
